d227048f4e2a4079d890b902dbf6bef156581d6f
bensultan1985 <bensultan1985@gmail.com>
2024-09-02 17:36:22 -0400

    removed api file from ts

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
