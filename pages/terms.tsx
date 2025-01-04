6a5a13fe6c6dc4cf7b94989aa40682da068df4ee
Ben <53099110+bensultan1985@users.noreply.github.com>
2025-01-04 17:03:07 -0500

    updated terms and about (#5)

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -1,3 +1,22 @@
+import { Title } from "@mantine/core";
+import styles from "../styles/Home.module.css";
 export default function Terms() {
-  return <div></div>;
+  return (
+    <main className={styles.main}>
+      <div>
+        <Title>Terms of Service</Title>
+        <p>
+          By using this website, you agree to the following terms of service:
+        </p>
+        <ul>
+          <li>Will not engage in illegal activity</li>
+          <li>Will not engage in inappropriate activity</li>
+        </ul>
+        <p>
+          Simple, right? Now, go have fun and stop reading so many boring legal
+          documents!
+        </p>
+      </div>
+    </main>
+  );
 }
